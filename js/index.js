9c0d1a22ffeedb63940ceb36ad22bcf651eed397
Santiago Font <santifonlop@hotmail.com>
2022-04-13 15:11:03 -0300

    arreglo de img de productos

--- js/index.js
+++ js/index.js
@@ -13,12 +13,12 @@
     } 
 
     const comidas = []
-            comidas.push(new productos(1,"Muzzarella", 170, "Porción de pizza con salsa de tomate, cubierta con ezquisita muzzarella", "../img/muzzarella.png"))
-            comidas.push(new productos(2,"Muzzarella a caballo", 265, "Porción de muzzarella + Porción de fainá 'a caballo'", "../img/musarellaCaballo.png"))
-            comidas.push(new productos(3,"Fugazzeta", 170, "Porción de pizza con cebolla picada y pequeños trozos de jamón y panceta, cubierta por una capa de exquisita muzzarella", "../img/fugazzeta.png"))
-            comidas.push(new productos(4,"Pizza", 95, "Porción de pizza con salsa artesanal, elaborada con tomates seleccionados de baja acidez y buena maduración", "../img/pizza.png"))
-            comidas.push(new productos(5,"Fainá", 95, "Porción de fainá, hecho con harina de garbanzo, horneado en una exclusiva bandeja de cobre", "../img/faina.png"))
-            comidas.push(new productos(6,"Fainá con muzzarella", 170, "Porción de fainá con una exquisita capa de muzzarella", "../img/fainaMuzarella.png"))
+            comidas.push(new productos(1,"Muzzarella", 170, "Porción de pizza con salsa de tomate, cubierta con ezquisita muzzarella", "./img/muzzarella.png"))
+            comidas.push(new productos(2,"Muzzarella a caballo", 265, "Porción de muzzarella + Porción de fainá 'a caballo'", "./img/musarellaCaballo.png"))
+            comidas.push(new productos(3,"Fugazzeta", 170, "Porción de pizza con cebolla picada y pequeños trozos de jamón y panceta, cubierta por una capa de exquisita muzzarella", "./img/fugazzeta.png"))
+            comidas.push(new productos(4,"Pizza", 95, "Porción de pizza con salsa artesanal, elaborada con tomates seleccionados de baja acidez y buena maduración", "./img/pizza.png"))
+            comidas.push(new productos(5,"Fainá", 95, "Porción de fainá, hecho con harina de garbanzo, horneado en una exclusiva bandeja de cobre", "./img/faina.png"))
+            comidas.push(new productos(6,"Fainá con muzzarella", 170, "Porción de fainá con una exquisita capa de muzzarella", "./img/fainaMuzarella.png"))
     
     
             let padre = document.getElementsByClassName("productos")[0]

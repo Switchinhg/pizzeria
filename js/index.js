4583ac3ff6f1586492b1f64d7f4cb25bb5eaff77
Santiago Font <santifonlop@hotmail.com>
2022-04-19 22:18:13 -0300

    arreglos

--- js/index.js
+++ js/index.js
@@ -8,18 +8,31 @@
             this.img = img
         }
 
-        
-
     } 
+    class carritos{
+        constructor(id,nombrecomida, precio, descripcion, img ){
+            this.id = id,
+            this.nombrecomida = nombrecomida,
+            this.precio = precio,
+            this.descripcion = descripcion,
+            this.img = img
+        }
 
-    const comidas = []
-            comidas.push(new productos(1,"Muzzarella", 170, "Porción de pizza con salsa de tomate, cubierta con ezquisita muzzarella", "./img/muzzarella.png"))
-            comidas.push(new productos(2,"Muzzarella a caballo", 265, "Porción de muzzarella + Porción de fainá 'a caballo'", "./img/musarellaCaballo.png"))
-            comidas.push(new productos(3,"Fugazzeta", 170, "Porción de pizza con cebolla picada y pequeños trozos de jamón y panceta, cubierta por una capa de exquisita muzzarella", "./img/fugazzeta.png"))
-            comidas.push(new productos(4,"Pizza", 95, "Porción de pizza con salsa artesanal, elaborada con tomates seleccionados de baja acidez y buena maduración", "./img/pizza.png"))
-            comidas.push(new productos(5,"Fainá", 95, "Porción de fainá, hecho con harina de garbanzo, horneado en una exclusiva bandeja de cobre", "./img/faina.png"))
-            comidas.push(new productos(6,"Fainá con muzzarella", 170, "Porción de fainá con una exquisita capa de muzzarella", "./img/fainaMuzarella.png"))
-    
+
+
+    }
+    const carrito = [/* comidas a pedir */]
+
+    const comidas = [{id: 1,nombrecomida:"Muzzarella",precio: 170,descripcion: "Porción de pizza con salsa de tomate, cubierta con ezquisita muzzarella", img:"./img/muzzarella.png"},
+                    {id: 2,nombrecomida:"Muzzarella a caballo",precio: 265,descripcion: "Porción de muzzarella + Porción de fainá 'a caballo'", img:"./img/musarellaCaballo.png"},
+                    {id: 3,nombrecomida:"Fugazzeta",precio: 170,descripcion: "Porción de pizza con cebolla picada y pequeños trozos de jamón y panceta, cubierta por una capa de exquisita muzzarella", img:"./img/fugazzeta.png"},
+                    {id: 4,nombrecomida:"Pizza",precio: 95,descripcion: "Porción de pizza con salsa artesanal, elaborada con tomates seleccionados de baja acidez y buena maduración", img:"./img/pizza.png"},
+                    {id: 5,nombrecomida:"Fainá",precio: 95,descripcion: "Porción de fainá, hecho con harina de garbanzo, horneado en una exclusiva bandeja de cobre", img:"./img/faina.png"},
+                    {id: 6,nombrecomida:"Fainá con muzzarella",precio: 170,descripcion: "Porción de fainá con una exquisita capa de muzzarella", img:"./img/fainaMuzarella.png"},
+]
+
+
+/*  comidas.push(new productos(1,"Muzzarella", 170, "Porción de pizza con salsa de tomate, cubierta con ezquisita muzzarella", "./img/muzzarella.png")) EJEMPLO DE PUSH PORQUE VOY A AGREGAR QUE EL DUEÑO AGREGUE COMIDAS*/
     
             let padre = document.getElementsByClassName("productos")[0]
     for (const comida of comidas) {
@@ -43,7 +56,6 @@
                             </div>
                     `
 
-                    console.log("entre pero no hice nada se ve xd")
         padre.appendChild(prod)
 
     }

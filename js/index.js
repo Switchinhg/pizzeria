8458e19726160433b8ae8a8fffd5b13181d525ee
Santiago Font <santifonlop@hotmail.com>
2022-04-21 05:24:23 -0300

    ahora productos agregados al carrito se muestran bien

--- js/index.js
+++ js/index.js
@@ -9,19 +9,20 @@ class productos {
 
 }
 class carritos {
-    constructor(id, nombrecomida, precio, descripcion, img) {
+    constructor(id, nombrecomida, precio, descripcion, img, gustos, cantidad) {
         this.id = id,
             this.nombrecomida = nombrecomida,
             this.precio = precio,
             this.descripcion = descripcion,
-            this.img = img
+            this.img = img,
+            this.gustos = gustos,
+            this.cantidad = cantidad
     }
 
 
 
 }
 const carrito = [ /* comidas a pedir */ ]
-
 const comidas = [{
         id: 1,
         nombrecomida: "Muzzarella",
@@ -145,45 +146,126 @@ btnmenos.onclick = () => {
     }
 }
 
+/* Cuando apreta el boton comprar lo pone en el carrito */
+
 const btnagregar = document.getElementById("btnagregar")
 btnagregar.onclick = () => {
     const nmb = document.getElementById("productoname").innerHTML
     localStorage.setItem(nmb, suma)
-
+    
     const prdcrrito = document.getElementsByClassName("sec_carrito")[0]
 
     for (const i of comidas) {
         if (i.nombrecomida == nmb) {
-            let prod = document.createElement("div")
-            prod.classList.add('producto_carrito')
 
-            prod.innerHTML = `
-                                    
-                        <div class="producto_borrar">
-                            <a href="javascript:void(0)" onclick="sacarProducto()">
-                                <i class="fa-solid fa-xmark"></i>
-                            </a>
-                        </div>
-                        <div class="carrito_img">
-                            <img src="./img/faina.png" alt="">
-                        </div>
-                        <div class="carrito-pedido-gustos-precio">
-                            <div class="carrito-pedidogusto">
 
-                                <p><b> ${suma +" "+ i.nombrecomida}</b></p>
-                                 <p class="gustos">gustos (Ej: albaca, oregano etc)</p> 
-                            </div>
-                            <div class="carrito_precio">
-
-                                <p>$${i.precio*suma}</p>
-                            </div>
-                        </div>
-                                    
-                            `
-                            prdcrrito.appendChild(prod)
+                            /* agrega lo elegido a comprar en el array carrito */
+            carrito.push(new carritos(1, i.nombrecomida, i.precio * suma, i.descripcion, i.img, i.gustos , suma))
+                /* guarda el array como json */
+                
+                const carritoJSON = JSON.stringify(carrito)
+                localStorage.setItem("carritoJSON" , carritoJSON)
+                actualizarcarritonavbar()
+                
 
         }
 
     }
     salirCompra()
+}
+
+const loadcarrito = document.getElementsByClassName("loadcarrito")
+
+for(let i =0 ; i<loadcarrito.length; i++){
+    loadcarrito[i].addEventListener("click", e => {
+    cargarcarrito()
+})
+}
+
+actualizarcarritonavbar()
+
+
+function cargarcarrito() {
+
+    
+
+        
+        const prodJSON = localStorage.getItem("carritoJSON")
+        const prodJSONobject = JSON.parse(prodJSON)
+
+        let total = 0
+        
+        for (const i of prodJSONobject) {
+            
+            
+            const prdcrrito = document.getElementsByClassName("sec_carrito")[0]
+            
+                let prod = document.createElement("div")
+                prod.classList.add('producto_carrito')
+                
+                prod.innerHTML = `
+                                        
+                            <div class="producto_borrar">
+                                <a href="javascript:void(0)" onclick="sacarProducto()">
+                                    <i class="fa-solid fa-xmark agarrar"></i>
+                                </a>
+                            </div>
+                            <div class="carrito_img">
+                                <img src="${i.img}" alt="">
+                            </div>
+                            <div class="carrito-pedido-gustos-precio">
+                                <div class="carrito-pedidogusto">
+    
+                                    <p><b> ${i.cantidad +" "+ i.nombrecomida}</b></p>
+                                    <p class="gustos">gustos (Ej: albaca, oregano etc)</p> 
+                                </div>
+                                <div class="carrito_precio">
+    
+                                    <p>$${i.precio*suma}</p>
+                                </div>
+                            </div>
+                                        
+                                `
+                                total += i.precio*suma 
+                                prdcrrito.appendChild(prod)
+                                document.getElementById("carritoTotal").innerHTML = "Total: <span style='color:rgb(7, 167, 7)'>$" + total + "</span>"
+                                
+
+            
+        }
+}
+
+function actualizarcarritonavbar(){
+
+    const prodJSON = localStorage.getItem("carritoJSON")
+    const prodJSONobject = JSON.parse(prodJSON)
+
+    document.getElementById("carrito-items").innerHTML =prodJSONobject.length
+}
+
+/* borrar el carrito cada vez que sale, para evitar que se buge */
+
+const prdcrrito = document.getElementsByClassName("sec_carrito")[0]
+
+const salircarrito = document.getElementById("salircarrito")
+salircarrito.addEventListener("click", e =>{
+    prdcrrito.innerHTML = ""
+})
+
+
+const xs = document.getElementsByClassName('agarrar')
+function sacarProducto() {
+
+
+
+    for(let i =0 ; i<xs.length; i++){
+        xs[i].addEventListener("click", e => {
+            e.currentTarget.parentNode.parentNode.parentNode.remove()
+    })
+    }
+
+
+    /* return this.parentNode.remove()
+    /* return this.parentNode.remove(); */ 
+    
 }

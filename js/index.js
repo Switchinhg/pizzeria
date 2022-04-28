e08234ca610535ae5108e0f8058f3e0a4284ef2b
Santiago Font <santifonlop@hotmail.com>
2022-04-28 00:52:25 -0300

    arreglado carrito + seleccion de producto

--- js/index.js
+++ js/index.js
@@ -6,23 +6,12 @@ class productos {
             this.descripcion = descripcion,
             this.img = img
     }
-
-
+    /* Tengo pensado hacer que el "dueño" de la app pueda agregar comidas a gusto */
 }
-class carritos {
-    constructor(id, nombrecomida, precio, descripcion, img, gustos, cantidad) {
-        this.id = id,
-            this.nombrecomida = nombrecomida,
-            this.precio = precio,
-            this.descripcion = descripcion,
-            this.img = img,
-            this.gustos = gustos,
-            this.cantidad = cantidad
-    }
 
-}
 
 let carrito = [ /* comidas a pedir */ ]
+let pedidosAnteriores = [ /* Pedidos viejos */]
 
 let padre = document.getElementsByClassName("productos")[0]
 const btnagregar = document.getElementById("btnagregar")
@@ -36,130 +25,93 @@ let carritoTotal = document.getElementById("carritoTotal")
 mostrarProductos(comidas);
 
 function mostrarProductos(array) {
+    padre.innerHTML = "";
+    array.forEach(comida => {
+        
+                let prod = document.createElement("div")
+                prod.classList.add('producto')
+                prod.innerHTML += `
+                                        
+                                        <div class="prodImg">
+                                            <img src="${comida.img}" alt="">
+                                        </div>
+                                        <div class="info">
+                                            <p id="prod${comida.id}" class="nombrecomidas">${comida.comida}</p>
+                                            <p>${comida.descripcion}</p>
+                                        </div>
+                                        <div class="precio">
+                                            <p>$${comida.precio}</p>
+                                        </div>
+                                        
+                                `
+        
+                padre.appendChild(prod)
 
-    for (const comida of array) {
 
-        let prod = document.createElement("div")
-        prod.classList.add('producto')
 
-        prod.innerHTML = `
-                                
-                                <div class="prodImg">
-                                    <img src="${comida.img}" alt="">
-                                </div>
-                                <div class="info">
-                                    <p id="prod${comida.id}" class="nombrecomidas">${comida.comida}</p>
-                                    <p>${comida.descripcion}</p>
-                                </div>
-                                <div class="precio">
-                                    <p>$${comida.precio}</p>
-                                </div>
-                                
-                        `
+            prod.addEventListener("click",()=>{
+                abrirCompra(comida.id)
 
-        padre.appendChild(prod)
+            })
+    });
+}
 
+let suma = 1
+btnagregar.addEventListener("click", () => {
+    let prod = JSON.parse(localStorage.getItem("producto"))
+    const cantcompra2 = document.getElementById("cantidadcompra").innerHTML
+    console.log(cantcompra2)
+    agregarCarrito(prod.id, cantcompra2)
+    actualizarCarrito()
+    document.getElementById("cantidadcompra").innerHTML = 1
+    suma = 1
+    salirCompra()
+
+}) 
+
+const btnmas = document.getElementsByClassName("botonchico")[1]
+const btnmenos = document.getElementsByClassName("botonchico")[0]
+const cantidadcompra = document.getElementById("cantidadcompra")
+
+
+btnmas.onclick = () => {
+    suma++
+    cantidadcompra.innerHTML = suma
+}
+btnmenos.onclick = () => {
+    if (suma != 1) {
+        suma--
+        cantidadcompra.innerHTML = suma
     }
 }
 
-
-
-/* Al hacer clic en un producto abre la la pantalla y carga el producto */
-
-
-
-
-
- const variProductos = document.getElementsByClassName("producto")
-/*
-for (const is of variProductos) {
-        is.addEventListener("click", e =>{
-            abrirCompra()
-            
-            for (const i of comidas) {
-                const produclicked = document.getElementById(`prod${i.id}`)
-                console.log(produclicked.id)
-                if(produclicked.id === i.id){
-                    document.getElementById("productoname").innerHTML = i.comida
-                    document.getElementById("imgproducto").src = `${i.imgg}`
-                    document.getElementById("nombreProducto").innerHTML = i.comida
-                    document.getElementById("precioProducto").innerHTML = "$" + i.precio
-                    document.getElementById("detalleprod").innerHTML = i.descripcion
-
-                    btnagregar.addEventListener("click", () => {
-                        agregarCarrito(i.id)
-        
-                            //buscamos el producto en nuestra BD
-                        let productoAgregar = comidas.find((elemento) => elemento.id == i.id)
-                        let prod = comidas.find((elemento) => elemento.id == i.id)
-                        localStorage.setItem("producto", JSON.stringify(productoAgregar))
-                    }) 
-
-                }
-                
-            }
-        })
-}
- */
-
-for (const i of variProductos) {
-    i.addEventListener("click", e => {
-        const ClickComida = i.getElementsByClassName("nombrecomidas")[0].innerHTML
-        abrirCompra()
-        const precioProducto = 0
-        const infoProducto = ""
-        for (const i of comidas) {
-
-            if (i.comida == ClickComida) {
-                document.getElementById("productoname").innerHTML = i.comida
-                document.getElementById("imgproducto").src = `${i.imgg}`
-                document.getElementById("nombreProducto").innerHTML = i.comida
-                document.getElementById("precioProducto").innerHTML = "$" + i.precio
-                document.getElementById("detalleprod").innerHTML = i.descripcion
-
-                
-                /* /*  Que hace? Agregar  */
-                btnagregar.addEventListener("click", () => {
-                    agregarCarrito(i.id)
-    
-                        //buscamos el producto en nuestra BD
-                    let productoAgregar = comidas.find((elemento) => elemento.id == i.id)
-                    let prod = comidas.find((elemento) => elemento.id == i.id)
-                    localStorage.setItem("producto", JSON.stringify(productoAgregar))
-                }) 
-
-            }
-        }
-        
-
-    })
-}
-
 /* agregar el producto clickeado al carrito */
-function agregarCarrito(id) {
-
+function agregarCarrito(id,num) {
     let esta = carrito.find((item) => item.id == id)
-    if (esta) { // se va al else con -> false, null, undefind y " " 
-        esta.cantidad = esta.cantidad + 1;
-        
-        console.log(esta.cantidad + "entre a agregar carrito")
-        /* Continuar */
+    console.log("este es el id en agregar carrit"+ id)
+    if (esta) { // se va al else con -> false, null, undefind y " "
+        if(num != 1){
+            esta.cantidad =+  num
+        } else{
+            esta.cantidad++
+        }
+
         document.getElementById(`prod${esta.id}`).innerHTML = ` <p id="prod${esta.id}"><b> ${esta.cantidad +" "+ esta.comida}</b></p>`
-        actualizarCarrito();
 
     } else {
         let prod = comidas.find((elemento) => elemento.id == id)
-        prod.cantidad = 1 //le agrego una propiedad "cantidad"
-
+        if(num != 1){
+            prod.cantidad =  num
+        } else{
+            prod.cantidad = 1 //le agrego una propiedad "cantidad"
+        }
+        console.log("entre en else")
         carrito.push(prod) // lo guardo en mi array
 
-        /*  actualizarCarrito() */
-        actualizarCarrito();
         cargarcarrito(prod)
     }
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
-
 
 function cargarcarrito(prod) {
     const prdcrrito = document.getElementsByClassName("sec_carrito")[0]
@@ -202,13 +154,11 @@ function actualizarCarrito() {
 
 
 function recuperar() {
-    let recuperarLS = JSON.parse(localStorage.getItem("carrito")); // si no existe, devuele null
-    console.log("entre en recuperer")
+    let recuperarLS = JSON.parse(localStorage.getItem("carrito")) || []// si no existe, devuele null
     if (recuperarLS) {
         console.log(recuperarLS)
         recuperarLS.forEach((el) => {
             cargarcarrito(el);
-            console.log("soy el EL de recuperar",el)
             carrito.push(el);
             actualizarCarrito();
         });
--- js/menus.js
+++ js/menus.js
@@ -1,14 +1,29 @@
-function abrirCompra(){
+function abrirCompra(id){
+
+    let prod = comidas.find((elemento) => elemento.id == id)
+    console.log(prod)
+    document.getElementById("productoname").innerHTML = prod.comida
+    document.getElementById("imgproducto").src = `${prod.imgg}`
+    document.getElementById("nombreProducto").innerHTML = prod.comida
+    document.getElementById("precioProducto").innerHTML = "$" + prod.precio
+    document.getElementById("detalleprod").innerHTML = prod.descripcion
+
+    localStorage.setItem("producto", JSON.stringify(prod))
+
     document.getElementsByClassName("pedido-wrap")[0].style.display = "flex"
     document.getElementById("body").style.overflow = "hidden"
     document.getElementById("header-sidebar").style.left = "-250px"
+
+
+
 }
+
 function salirCompra(){
     document.getElementsByClassName("pedido-wrap")[0].style.display = "none"
     document.getElementById("body").style.overflow = "auto"
     document.getElementById("imgproducto").src= "./img/placeholder.png"
-    suma =1
-    cantidadcompra.innerHTML = suma
+
+
 }
 function abrirSidebar() {
     document.getElementById("header-sidebar").style.left = "0"
